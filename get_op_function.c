9d0595c142a56816b2c0c1e606c0b06985492498
Mulugeta Achiso <anem1351@gmail.com>
2022-07-17 06:32:00 -0700

    get operation func

--- get_op_function.c
+++ get_op_function.c
@@ -14,7 +14,19 @@ void (*get_op_func(char *s))(stack_t**, unsigned int)
 	instruction_t ops[] = {
 		{"push", op_push},
 		{"pall", op_pall},
-		{"pint", op_pint}, /*Add new functions on here*/
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{"sub", op_sub},
+		{"div", op_div},
+		{"mul", op_mul},
+		{"mod", op_mod},
+		{"pchar", op_pchar},
+		{"pstr", op_pstr},
+		{"rotl", op_rotl},
+		{"rotr", op_rotr},
 		{NULL, NULL}
 	};
 	int i = 0;
