98d76d0d84725ed98358d8cbce5f292d9249ac87
Mulugeta Achiso <anem1351@gmail.com>
2022-07-17 05:15:47 -0700

    strtol function

--- monty_functionalities.c
+++ monty_functionalities.c
@@ -9,17 +9,17 @@
 void op_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
-	int n;
+	int num;
 
 	if (stack == NULL)
 	{
-		fprintf(stderr, "L%d: stack not found\n", line_number);
+		fprintf(stderr, "L%u: stack not found\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	if (operand == NULL || _atoi(operand, &n) == -1)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -31,10 +31,10 @@ void op_push(stack_t **stack, unsigned int line_number)
 		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-
+	num = _strtol(op, line_number);
 	new->next = *stack;
 	new->prev = NULL;
-	new->n = n;
+	new->n = num;
 
 	if (*stack)
 		(*stack)->prev = new;
@@ -69,12 +69,58 @@ void op_pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 	(void)line_number;
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
 	printf("%d\n", temp->n);
+}
+
+/**
+ * instruction_pop - removes top element of stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ * Return: nothing
+ */
+void instruction_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node  = *stack;
+	(*stack) = (*stack)->next;
+	free(node);
+}
+
+/**
+ * instruction_swap - swaps 2 elements of the stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ */
+void instruction_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void)(line_number);
+	if (!(*stack) || !((*stack)->next))
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	(*stack)->prev = (*stack)->next;
+	(*stack)->next = tmp->next;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack) = (*stack)->prev;
 }
