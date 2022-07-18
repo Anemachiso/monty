75cb22959b3b0f5000dcdf45d608aa869863292b
Mulugeta Achiso <anem1351@gmail.com>
2022-07-18 04:11:40 -0700

    check error

--- aux_functions.c
+++ aux_functions.c
@@ -63,3 +63,21 @@ void cleanStack(stack_t **stack)
 	fclose(global.fd);
 	free(global.line);
 }
+
+/**
+ * free_dlistint - Free a list.
+ * @stack: Head node.
+ * Return: Nothing.
+ */
+void free_dlistint(stack_t *stack)
+{
+	stack_t *current = NULL;
+
+	current = stack;
+
+	if (current != NULL)
+	{
+		free_dlistint(current->next);
+		free(current);
+	}
+}
